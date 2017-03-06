841f43d5d77e595eb5837f3f527d6035d4536cde
NashvillePublicLibrary <james.staub@nashville.gov>
2017-03-06 07:28:43 -0600

    Changes PIN cracking method from PatronAPI to John

--- format_pin.sh
+++ format_pin.sh
@@ -4,25 +4,13 @@
 
 # TO DO: LOOKUP PATRON BARCODE?
 
-perl -F'\|' -lane '
-# ADD FINAL PIPE
-	$F[1].="|";
-# Transform 7 digit record number to Millennium dot number with check digit
-# Check digit calculation as per IGR #105781
-# http://csdirect.iii.com/manual_2009b/rmil_records_numbers.html
-	if ($F[0] =~ /^\d+$/) { # DO NOT MUNGE HEADER ROW .pPATRONID0|PIN0...
-		@j = split //, $F[0];
-                my $sum;
-                for my $k (0 .. $#j) {
-                        $sum += $j[$k]*(8-$k);
-                }
-                $checksum = $sum % 11;
-                if ($checksum == 10) { $checksum = "x"; }
-                $F[0] = ".p" . $F[0] . $checksum;
-	}
-print join q/|/, @F;' ../data/validUserPins.txt > ../data/PATRON_PIN.txt
+# FOR NOW...
+cd john-1.8.0/run/
+./john --show ../../../data/20170217_millennium_patrons_with_pins.txt | awk -F: '{print "."$1"|"$2"|"}' > ../../../data/PATRON_PIN.txt
+cd ../../
 
-# TO DO: ADD HEADER PATRON|PIN
-# TO DO: ADD PATRON BARCODE?
+# ADD CARL.X HEADER PATRON|PIN
+perl -pi -e 'print "PATRON|PIN|\n" if $. == 1' ../data/PATRON_PIN.txt
+
 # TO DO: rm files
 
