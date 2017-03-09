c54c9efc3882190aa7624ef5f4959f65e00c8b39
NashvillePublicLibrary <james.staub@nashville.gov>
2017-03-09 06:18:32 -0600

    Corrects header on PATRON_PIN.txt

--- format_pin.sh
+++ format_pin.sh
@@ -4,13 +4,13 @@
 
 # TO DO: LOOKUP PATRON BARCODE?
 
-# FOR NOW...
+# JOHN
 cd john-1.8.0/run/
-./john --show ../../../data/20170217_millennium_patrons_with_pins.txt | awk -F: '{print "."$1"|"$2"|"}' > ../../../data/PATRON_PIN.txt
+./john --incremental=digits ../../../data/millennium_extract-patronsWithPins.txt
+./john --show ../../../data/millennium_extract-patronsWithPins.txt | awk -F: '{print "."$1"|"$2"|"}' > ../../../data/PATRON_PIN.txt
 cd ../../
 
-# ADD CARL.X HEADER PATRON|PIN
-perl -pi -e 'print "PATRON|PIN|\n" if $. == 1' ../data/PATRON_PIN.txt
+# ADD CARL.X HEADER PATRONID|PIN
+perl -pi -e 'print "PATRONID|PIN|\n" if $. == 1' ../data/PATRON_PIN.txt
 
 # TO DO: rm files
-
