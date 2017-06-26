dc8c82ae059b3faa86c02e9231f5cbd82aabb790
NashvillePublicLibrary <james.staub@nashville.gov>
2017-06-26 13:23:01 -0500

    Eliminates ITEM_NOTE REPAIRED
    https://trello.com/c/Y0y4BWyF

--- format_item_note.sh
+++ format_item_note.sh
@@ -7,6 +7,11 @@
 perl -F'\|' -lane '
 # DO NOT MIGRATE MISSING FROM HOLDSHELF
 	if($F[4] =~ m/^.*(missing|not found).*hold[- ]?shelf.*$/i) {next;}
+## MIGRATE RESURFACED
+##	if ($F[4] =~ s/^.*resurfaced.*$/i)
+# DO NOT MIGRATE REPAIRED
+	if($F[4] =~ m/^.*repaired.*$/i) {next;}
+	if($F[4] =~ m/^tested.*\b(fine|ok|works)\b.*/i) {next;}
 
 print join q/|/, @F;' ../data/ITEM_NOTE.txt > ../data/ITEM_NOTE_CLEANER.txt
 #mv -f ../data/ITEM_NOTE_CLEANER.txt ../data/ITEM_NOTE.txt
